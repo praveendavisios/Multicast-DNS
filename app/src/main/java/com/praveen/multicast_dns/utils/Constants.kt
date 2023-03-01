fdd23af900f281b415e8c0d664e58ae9dd4f81c7
praveen <praveendavis7@gmail.com>
2023-03-01 21:53:52 +0530

    testing with port 80

--- app/src/main/java/com/praveen/multicast_dns/utils/Constants.kt
+++ app/src/main/java/com/praveen/multicast_dns/utils/Constants.kt
@@ -2,6 +2,6 @@ package com.praveen.multicast_dns.utils
 
 object Constants {
     const val SERVICE_TYPE = "_http._tcp."
-    const val SERVICE_NAME = "MyService002"
-    const val PORT = 8080
+    const val SERVICE_NAME = "MyService001"
+    const val PORT = 80
 }
